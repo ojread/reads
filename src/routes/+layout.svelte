dca038e94683c0c9f013d7045e5abb30edc5d323
Oliver Read <oliver@reads.org.uk>
2024-12-18 21:39:06 +0000

    Add Flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,43 @@
 <script lang="ts">
   import "../app.css";
+
+  import {
+    DarkMode,
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
+
   let { children } = $props();
 </script>
 
-{@render children()}
+<div
+  class="relative min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white"
+>
+  <header>
+    <Navbar color="default" class="fixed">
+      <NavBrand href="/">
+        <span
+          class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+          >reads.org.uk</span
+        >
+      </NavBrand>
+      <NavHamburger />
+      <NavUl>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/#">About</NavLi>
+        <NavLi href="/#">Pricing</NavLi>
+        <NavLi href="/#">Contact</NavLi>
+      </NavUl>
+      <DarkMode />
+    </Navbar>
+  </header>
+
+  <main class="pt-24">
+    <div class="container mx-auto">
+      {@render children()}
+    </div>
+  </main>
+</div>
